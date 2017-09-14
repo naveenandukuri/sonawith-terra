5359c5f7f6d4ac98efe973a9b134c79a0154bccf
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-09-14 21:43:53 +0530

    Updating sonarqube

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Source Common Functions
+curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
+#source /root/scripts/common-functions.sh
+source /tmp/common-functions.sh
+
+## Checking Root User or not.
+CheckRoot
+
+## Checking SELINUX Enabled or not.
+CheckSELinux
+
+## Checking Firewall on the Server.
+CheckFirewall
+
+## Downloading Java
+DownloadJava 8
+
+## Installing Java
+yum localinstall $JAVAFILE -y &>/dev/null
+if [ $? -eq 0 ]; then 
+	success "JAVA Installed Successfully"
+else
+	error "JAVA Installation Failure!"
+	exit 1
+fi
+
