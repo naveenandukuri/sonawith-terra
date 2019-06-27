860d5bd864a5596cd51169be92fb6f2c77c05884
Raghu K <raghukx@gmail.com>
2019-06-27 14:48:11 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,14 @@ provider "aws" {}
 resource "aws_instance" "sonar" {
     ami             = "ami-0503db1a235b15e3f"
     instance_type   = "t2.medium"
+    vpc_security_group_ids = ["sg-ff6b6ab6"]
     
      provisioner "remote-exec" {
         connection {
             type     = "ssh"
             user     = "centos"
             private_key = "${file("/home/centos/.ssh/id_rsa")}"
-            vpc_security_group_ids = ["sg-ff6b6ab6"]
+            
           }
         inline = [
         "curl -s https://raw.githubusercontent.com/linuxautomations/sonarqube/master/install.sh | sudo bash",
