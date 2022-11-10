5a34e80831d18630da1b1c9e59f0051fac6a7f83
naveenandukuri <102795429+naveenandukuri@users.noreply.github.com>
2022-11-10 10:43:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,8 +19,8 @@ if [ $? -ne 0 ]; then
 	## Downloading Java
 	#DownloadJava 8
 	## Installing Java
-	#yum install /opt/jdk* -y &>/dev/null
-	yum install java -y &>/dev/null
+	#apt-get install /opt/jdk* -y &>/dev/null
+	apt-get install java -y &>/dev/null
 	if [ $? -eq 0 ]; then 
 		success "JAVA Installed Successfully"
 	else
@@ -32,64 +32,7 @@ else
 fi
 
 
-## Downloading MYSQL Repositories and MySQL Server
-yum install https://kojipkgs.fedoraproject.org/packages/python-html2text/2016.9.19/1.el7/noarch/python2-html2text-2016.9.19-1.el7.noarch.rpm -y &>/dev/null
-MYSQLRPM=$(curl -s http://repo.mysql.com/ | html2text | grep el7 | grep mysql57| tail -1 | sed -e 's/(/ /g' -e 's/)/ /g' | xargs -n1 | grep ^mysql)
-MYSQLURL="http://repo.mysql.com/$MYSQLRPM"
 
-if [ ! -f /etc/yum.repos.d/mysql-community.repo ]; then 
-
-	yum install $MYSQLURL -y &>/dev/null
-	if [ $? -eq 0 ]; then 
-		success "Successfully Configured MySQL Repositories"
-	else
-		error "Error in Configuring MySQL Repositories"
-		exit 1
-	fi
-else
-	warning "MySQL Repositories are already Configured"
-fi     
-
-## Installing MySQL Server
-yum install mysql-server -y &>/dev/null
-if [ $? -eq 0 ]; then 
-	success "Successfully Installed MySQL Server"
-else
-	error "Error in Installing MySQL Server"
-	exit 1
-fi
-
-echo 'vm.max_map_count=262144' >/etc/sysctl.conf 
-sysctl -p &>/dev/null
-
-echo '* - nofile 80000' >>/etc/security/limits.conf
-
-## Set Root Password and reset it
-sed -i -e '/query_cache_size/ d' -e '$ a query_cache_size = 15M' /etc/my.cnf 
-systemctl enable mysqld &>/dev/null
-systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
-systemctl restart mysqld
-mysql -e "use mysql;update user set authentication_string=password(''), plugin='mysql_native_password' where user='root';"
-systemctl unset-environment MYSQLD_OPTS
-systemctl restart mysqld
-mysql --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'P@$$w0rD'"
-systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
-systemctl restart mysqld
-mysql -e "use mysql;update user set authentication_string=password(''), plugin='mysql_native_password' where user='root';"
-systemctl unset-environment MYSQLD_OPTS
-systemctl restart mysqld
-mysql -e "grant all privileges on sonarqube.* to 'sonarqube'@'localhost' identified by 'password';" &>/dev/null
-mysql -e 'uninstall plugin validate_password;' &>/dev/null
-
-## Creating DB and User access
-wget https://raw.githubusercontent.com/linuxautomations/sonarqube/master/sonarqube.sql -O /tmp/sonarqube.sql &>/dev/null
-mysql </tmp/sonarqube.sql
-if [ $? -eq 0 ]; then
-	success "Successfully Created DB and User access"
-else
-	error "Failed to create DB and User access"
-	exit 1
-fi
 
 ## Downloading SonarQube 
 #VER=$(curl -s https://sonarsource.bintray.com/Distribution/sonarqube/  | tail -n 10 | awk -F '[<,>]' '{print $5}' | grep zip$ |tail -1)
